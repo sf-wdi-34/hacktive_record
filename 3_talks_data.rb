d9ffbdb75835531adc6800defb46017c3e4b46b8
nathanallen <thenathanator@gmail.com>
2015-09-17 14:40:57 -0700

    refactor 3

--- 3_talks_data.rb
+++ 3_talks_data.rb
@@ -3,15 +3,11 @@ require 'ffaker'
 puts "Speakers"
 puts "========"
 5.times do
-  first_name = FFaker::Name.first_name
-  last_name = FFaker::Name.last_name
-  email = FFaker::Internet.free_email
-  phone = FFaker::PhoneNumber.short_phone_number
   speaker = {
-              first_name: first_name,
-              last_name: last_name,
-              email: email,
-              phone: phone
+              first_name: FFaker::Name.first_name,
+              last_name: FFaker::Name.last_name,
+              email: FFaker::Internet.free_email,
+              phone: FFaker::PhoneNumber.short_phone_number
             }
   puts speaker
 end
