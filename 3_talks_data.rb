98384228a25c3f23fd331785e391d401d5a6e1ae
nathanallen <thenathanator@gmail.com>
2015-09-17 14:33:26 -0700

    working on 3

--- 3_talks_data.rb
+++ 3_talks_data.rb
@@ -3,7 +3,17 @@ require 'ffaker'
 puts "Speakers"
 puts "========"
 5.times do
-  p ""
+  first_name = FFaker::Name.first_name
+  last_name = FFaker::Name.last_name
+  email = FFaker::Internet.free_email
+  phone = FFaker::PhoneNumber.short_phone_number
+  speaker = {
+              first_name: first_name,
+              last_name: last_name,
+              email: email,
+              phone: phone
+            }
+  puts speaker
 end
 
 puts "\nTalks"
