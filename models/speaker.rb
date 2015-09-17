5a8a396e3b0c98e253139054b94a8707ddd935e8
nathanallen <thenathanator@gmail.com>
2015-09-17 15:39:29 -0700

    solution to 4

--- models/speaker.rb
+++ models/speaker.rb
@@ -4,12 +4,6 @@ class Speaker < HacktiveRecord::Base
 
   attr_accessor :first_name, :last_name, :email, :id
 
-  def initialize(args)
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
-    @email = args[:email]
-  end
-
   def full_name
     "#{first_name} #{last_name}"
   end
