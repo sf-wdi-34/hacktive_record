8f8deaa39e1d1b14c25b8c9fd23ff77ad0419399
nathanallen <thenathanator@gmail.com>
2015-09-17 14:22:54 -0700

    update 2

--- 2_speakers.rb
+++ 2_speakers.rb
@@ -4,5 +4,6 @@ require 'ffaker'
   first_name = FFaker::Name.first_name
   last_name = FFaker::Name.last_name
   email = FFaker::Internet.free_email
-  puts "#{first_name} #{last_name}, #{email}"
+  phone = FFaker::PhoneNumber.short_phone_number
+  puts "#{first_name} #{last_name}, #{email}, #{phone}"
 end
